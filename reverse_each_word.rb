5595c0f1059acc52764ef11307a5797f36fc995d
Alethea Cary <aletheacary@gmail.com>
2019-05-16 22:15:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (string)
   array = string.split
   array2 = []
-  array.each do |element|
+  array.collect do |element|
     array2 << element.reverse
   end
   array2.join(" ")
